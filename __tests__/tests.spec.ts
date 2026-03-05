4b60b484afc65001befb47cec7aea1271998404f
Ilya Vasilyev <vasilyev.i123@gmail.com>
2026-03-05 11:53:10 +0300

    add test

--- __tests__/tests.spec.ts
+++ __tests__/tests.spec.ts
@@ -46,10 +46,10 @@ test.describe('Проверка на уязвимость пакетов', () =>
     expect(result.code).toEqual(0);
   });
 
-  // test('Аудит frontend пакетов', () => {
-  //   const result = shell.exec('npm audit', { cwd: `${process.env.GITHUB_WORKSPACE}/frontend`, silent: true });
-  //   expect(result.code).toEqual(0);
-  // });
+  test('Аудит frontend пакетов', () => {
+    const result = shell.exec('npm audit', { cwd: `${process.env.GITHUB_WORKSPACE}/frontend`, silent: true });
+    expect(result.code).toEqual(0);
+  });
 });
 
 test.describe('Проверка заказов', () => {
